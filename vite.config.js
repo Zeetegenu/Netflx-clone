a85a9a0f3acdc1dd78ca31651d16f52352253fff
Zeetegenu <zebibtegenu@outlook.com>
2024-10-04 00:51:24 -0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Netflx-Clone-2024/',
+  base: '/Netflx-clone/',
   plugins: [react()],
 })
