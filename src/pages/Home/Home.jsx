30c5313e4402799a48f9ae96bacfc874b8ada4d5
Zeetegenu <zebibtegenu@outlook.com>
2024-10-24 23:33:12 -0400

    Deploying it again

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner'
 import RowList from '../../components/RowList/RowList'
 
-
+// import requests from '../../utils/requests'
 const Home = () => {
   return (
     <>
